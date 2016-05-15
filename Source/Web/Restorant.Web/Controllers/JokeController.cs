0fb5f2d0a30dbab0e0b69368e1c2fa8468158e1c
adachenski <adachenski@aol.com>
2016-05-16 00:46:02 +0300

    resume css improved and separate to different files

---
+++ Source/Web/Restorant.Web/Controllers/JokeController.cs
@@ -0,0 +1,32 @@
+﻿namespace Restorant.Web.Controllers
+{
+    using Data;
+    using Data.Common.Repository;
+    using Data.Models;
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using ViewModels.Jokes;
+    public class JokeController : Controller
+    {
+        private IDeletableEntityRepository<Joke> jokes;
+
+        public JokeController(IDeletableEntityRepository<Joke>j)
+        {
+            this.jokes = j;
+        }
+       
+
+        // GET: Joke
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult _JokePartial()
+        {
+             var jokes = this.jokes.All().OrderBy(x => Guid.NewGuid())
+                .Take(3).Select(x=>new JokeViewModel() {Content= x.Content })
+                .ToList();
+
+            return PartialView(jokes);
+        }
+    }
+}
