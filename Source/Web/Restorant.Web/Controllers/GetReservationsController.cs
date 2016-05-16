59b417820c86dd58927e703596d6b08b6ccc31e1
adachenski <adachenski@aol.com>
2016-05-16 03:18:11 +0300

    Overall UI improvement

--- Source/Web/Restorant.Web/Controllers/GetReservationsController.cs
+++ Source/Web/Restorant.Web/Controllers/GetReservationsController.cs
@@ -27,20 +27,20 @@
             var allReservations = this.reservations.All().Where(p => p.PersonId == currentUser)
                 .Project().To<GetReservationsVIewModel>();
 
-           // var dbPerson = from p in allReservations
-           //                where p.PersonId == currentUser
-           //                orderby p.ReservedFor descending
-           //                select p;
+            // var dbPerson = from p in allReservations
+            //                where p.PersonId == currentUser
+            //                orderby p.ReservedFor descending
+            //                select p;
 
-           // if (!allReservations.Any())
-           // {
-           //     ViewBag.getReservations = "No reservation yet!";
-           //     return this.View();
-           // }
+            // if (!allReservations.Any())
+            // {
+            //     ViewBag.getReservations = "No reservation yet!";
+            //     return this.View();
+            // }
 
             return this.View(allReservations);
         }
-        
+
         [HttpGet]
         public ActionResult Delete(int? id, int page = 1)
         {
@@ -57,9 +57,10 @@
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete (int ? id)
+        public ActionResult Delete(int? id)
         {
-            var postViewModel = this.reservations.All().Where(x => x.Id == id)
+            var postViewModel = this.reservations.All()
+                .Where(x => x.Id == id)
                .FirstOrDefault();
             this.reservations.Delete(postViewModel);
             this.reservations.SaveChanges();
