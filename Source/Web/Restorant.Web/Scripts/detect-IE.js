6c3e9d11e253fbb8175a628602aa7ca4055e8c02
adachenski <adachenski@aol.com>
2016-05-21 03:47:25 +0300

    Project Description Added to front page. detect-IE.js file added

---
+++ Source/Web/Restorant.Web/Scripts/detect-IE.js
@@ -0,0 +1,10 @@
+﻿var ua = window.navigator.userAgent;
+var msie = ua.indexOf('MSIE '),
+    trident = ua.indexOf('Trident/'),
+    edge = ua.indexOf('Edge/');
+if (edge > 0 || msie > 0 || trident > 0) {
+    $('#ie-style').attr("href", "Content/IE.css");
+    setTimeout(function () {
+        $('.scroll').slideDown("slow");
+    }, 6000);
+}
